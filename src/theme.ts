a5a016d205951951e8c44c574d90e4f7e655393f
Bom <aosldk09@naver.com>
2023-03-02 03:05:52 +0900

    react-hook-form을 사용하지 않을 경우

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  textColor: "white",
+  bgColor: "#636e72",
+  accentColor: "#d63031",
+  titleColor: "#00b894",
+  liBgColor: "#dfe6e9",
+  themeBg: "white",
+};
