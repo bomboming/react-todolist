17b5f528280aca465c019a86c3bbda92077d60f3
Bom <aosldk09@naver.com>
2023-03-02 03:27:48 +0900

    react-hook-form을 사용했을 시 줄어든 코드

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,30 +1,18 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register } = useForm();
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="write a to do list"
-        />
-        <button>add</button>
+      <form>
+        <input {...register("toDo")} placeholder="write a to do list" />
+        <button>Add</button>
       </form>
     </div>
   );
 }
 
+// register를 사용하면 onChange 이벤트 핸들러가 필요없다
 export default ToDoList;
