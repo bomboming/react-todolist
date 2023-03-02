29cb4ced9d605ebfbe47564e8b57e0227d42ef83
Bom <aosldk09@naver.com>
2023-03-02 11:01:42 +0900

    useRecoilState 사용법

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,24 @@
 import { useForm } from "react-hook-form";
+import {
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 interface IForm {
   toDo: string;
 }
 
+const toDoState = atom({
+  key: "toDo",
+  default: [],
+});
+
 function ToDoList() {
-  //useForm에서 필요한 것들을 꺼내 사용 할 수 있다.
+  // const value = useRecoilValue(toDoState);  리코일로 값을 받아옴
+  // const setValue = useSetRecoilState(toDoState)   받아온 값을 수정함
+  const [toDoValue, setToDoValue] = useRecoilState(toDoState); // 각각 필요할 땐 따로 쓰고 합쳐서 쓰고싶을 땐 useRecoilState를 사용하면 된다.
   const { register, handleSubmit, setValue } = useForm<IForm>();
   const handleValid = (data: IForm) => {
     setValue("toDo", "");
@@ -13,6 +26,8 @@ function ToDoList() {
 
   return (
     <div>
+      <h1>To Dos!</h1>
+      <hr />
       <form onSubmit={handleSubmit(handleValid)}>
         <input
           {...register("toDo", {
@@ -22,6 +37,7 @@ function ToDoList() {
         />
         <button>Add</button>
       </form>
+      <ul></ul>
     </div>
   );
 }
