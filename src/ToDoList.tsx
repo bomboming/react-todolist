d10cd43cd5deabf9b2acfa351ae0e5839358ec1b
Bom <aosldk09@naver.com>
2023-03-02 10:32:53 +0900

    useForm 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  toDo: string;
+}
+
 function ToDoList() {
-  const { register } = useForm();
+  //useForm에서 필요한 것들을 꺼내 사용 할 수 있다.
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    setValue("toDo", "");
+  };
 
   return (
     <div>
-      <form>
-        <input {...register("toDo")} placeholder="write a to do list" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder="Write a to do list"
+        />
         <button>Add</button>
       </form>
     </div>
   );
 }
 
-// register를 사용하면 onChange 이벤트 핸들러가 필요없다
 export default ToDoList;
